ec1d36a505c102a38917f56a88de32853c57d81d
katerinavlasova <derkamesser@yandex.ru>
2022-02-11 14:48:28 +0300

    GUI shows picture that was send to server

--- server/serverwindow.cpp
+++ server/serverwindow.cpp
@@ -1,5 +1,6 @@
 #include "serverwindow.h"
 #include "ui_serverwindow.h"
+#include <QPixmap>
 
 ServerWindow::ServerWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,8 +17,11 @@ ServerWindow::~ServerWindow()
     delete ui;
 }
 
-
+//show picture from server
 void ServerWindow::showPicture(QByteArray &bytePicture)
 {
+    QPixmap pic;
+    pic.loadFromData(bytePicture, "PNG");
+    ui->PictureLabel->setPixmap(pic);
     qDebug("show picture!! \n");
 }
